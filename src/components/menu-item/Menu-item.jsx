81cc3bca9a407705af4e15baa8da7fc9cfea6f10
tdieting <theresa.dietinger@dccs.at>
2022-02-01 21:38:00 +0100

    add styling for menu items

--- src/components/menu-item/Menu-item.jsx
+++ src/components/menu-item/Menu-item.jsx
@@ -3,9 +3,10 @@ import './Menu-item.styles.scss';
 
 export function MenuItem({title, imageUrl, size}) {
   return (
-    <div className={`${size} menuItem`} style={{backgroundImage: `url(${imageUrl}`}}>
+    <div className={`${size} menuItem`}>
+      <div className={'background-image'} style={{backgroundImage: `url(${imageUrl}`}}/>
       <div className={'content'}>
-        <h1 className={'title'}>{title}</h1>
+        <h1 className={'title'}>{title.toUpperCase()}</h1>
         <span className={'subtitle'}>SHOP NOW</span>
       </div>
     </div>
